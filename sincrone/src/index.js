29a2173c2fdea00953fa474059912d2bd0c41d6d
vlad <vlad@vlads-MacBook-Pro.local>
2017-07-28 10:19:03 +0400

    my-app

--- sincrone/src/index.js
+++ sincrone/src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route} from 'react-router-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import reducer from './reducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 
 
-const store = createStore(reducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+// const store = createStore(reducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+let store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
+
 
 
 ReactDOM.render(
